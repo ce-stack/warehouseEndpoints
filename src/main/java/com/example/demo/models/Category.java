a69221956095a827f0cda580a5d07b54d1e2d32a
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-06-02 10:29:09 +0300

    add category model

--- src/main/java/com/example/demo/models/Category.java
+++ src/main/java/com/example/demo/models/Category.java
@@ -1,4 +1,40 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
