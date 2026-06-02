143fd63e54e7e8d28f6bd923fe0ad5a111339ea1
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-06-02 10:25:42 +0300

    add user model

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,4 +1,93 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role_name")
+    private String role_name;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id" , nullable = false)
+    private Role role;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String password, String role_name, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role_name = role_name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role_name='" + role_name + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
